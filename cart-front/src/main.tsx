a46d7732c88632923fd4c384348446871eebc0ae
euan-lee <maplestory1419@naver.com>
2024-04-03 08:10:44 +0900

    feat:msw설정

--- cart-front/src/main.tsx
+++ cart-front/src/main.tsx
@@ -4,9 +4,8 @@ import { worker } from "./Mocks/browser.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.VITE_NODE_ENV === "development") {
   worker.start();
-  console.log("start!!");
 }
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
