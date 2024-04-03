0d81dd14d61bce4168e99273431fad5749290172
euan-lee <maplestory1419@naver.com>
2024-04-03 18:52:22 +0900

    feat:page생성

---
+++ cart-front/src/Pages/itemDetail.tsx
@@ -0,0 +1,3 @@
+export const itemDetail = () => {
+  return <>itemDetail</>;
+};
--- cart-front/src/main.tsx
+++ cart-front/src/main.tsx
@@ -4,7 +4,7 @@ import { worker } from "./Mocks/browser.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
-if (process.env.VITE_NODE_ENV === "development") {
+if (import.meta.env.VITE_NODE_ENV === "development") {
   worker.start();
 }
 ReactDOM.createRoot(document.getElementById("root")!).render(
