031ff77c45640fe7e32e83ffa64b2e5a79c6b5ca
euan-lee <maplestory1419@naver.com>
2024-06-06 20:34:36 +0900

    feat:cardbody 컴포넌트 인터페이스 수정

--- cart-front/src/ComponentsPrototype/Card/CardBody.tsx
+++ cart-front/src/ComponentsPrototype/Card/CardBody.tsx
@@ -1,6 +1,9 @@
 import { HTMLAttributes } from "react";
+import { ReactNode } from "@tanstack/react-router";
 
-interface CardBodyProps extends HTMLAttributes<HTMLDivElement> {}
+interface CardBodyProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
 
 const CardBody = ({ children, ...props }: CardBodyProps) => {
   return <div {...props}>{children}</div>;
