d6d26d0d12e905f49cc5eb453e3f93856e47a719
euan-lee <maplestory1419@naver.com>
2024-06-06 20:40:47 +0900

    feat:card 컴포넌트 인터페이스 수정

--- cart-front/src/ComponentsPrototype/Card/Card.tsx
+++ cart-front/src/ComponentsPrototype/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from "react";
-
+import { ReactNode } from "@tanstack/react-router";
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
