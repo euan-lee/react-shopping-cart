2221106851a30a03215cbee50f0d23ba8ae9ae44
euan-lee <maplestory1419@naver.com>
2024-06-06 20:41:34 +0900

    feat:cardfooter 컴포넌트 인터페이스 수정

--- cart-front/src/ComponentsPrototype/Card/CardFooter.tsx
+++ cart-front/src/ComponentsPrototype/Card/CardFooter.tsx
@@ -1,7 +1,9 @@
 import { HTMLAttributes } from "react";
-
 import { ReactNode } from "@tanstack/react-router";
-interface CardFooterProps extends HTMLAttributes<HTMLDivElement> {}
+
+interface CardFooterProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
 
 const CardFooter = ({ children, ...props }: CardFooterProps) => {
   return <div {...props}>{children}</div>;
