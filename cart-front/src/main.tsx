3d45a3956af990357d3dcc4fba65f6bda0533a42
euan-lee <maplestory1419@naver.com>
2024-08-12 11:28:19 +0900

    msw 설정

--- cart-front/src/main.tsx
+++ cart-front/src/main.tsx
@@ -30,10 +30,11 @@ declare module "@tanstack/react-router" {
 }
 
 async function enableMocking() {
+  /*
   if (process.env.NODE_ENV !== "development") {
     return;
   }
-
+*/
   const { worker } = await import("./Mocks/browser.ts");
 
   return worker.start();
